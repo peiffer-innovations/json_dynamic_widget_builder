4e8894a003a9a05c96e2a552ef78c616cbde8ede
Jeff Peiffer <jpeiffer@gmail.com>
2023-02-23 19:15:41 -0500

    Updates

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:json_dynamic_widget_builder/src/widgets/console.dart';
 import 'package:json_dynamic_widget_builder/src/widgets/horizontal_split_view.dart';
 import 'package:json_dynamic_widget_builder/src/widgets/results.dart';
-import 'package:json_dynamic_widget_builder/src/widgets/schema_view.dart';
 import 'package:json_dynamic_widget_builder/src/widgets/vertical_split_view.dart';
 import 'package:json_dynamic_widget_builder/src/widgets/widgets_panel.dart';
 
@@ -17,14 +16,10 @@ class HomePage extends StatelessWidget {
       body: VerticalSplitView(
         left: WidgetsPanel(),
         ratio: 0.2,
-        right: VerticalSplitView(
-          left: HorizontalSplitView(
-            bottom: Console(),
-            ratio: 0.85,
-            top: Results(),
-          ),
-          ratio: 0.77,
-          right: SchemaView(),
+        right: HorizontalSplitView(
+          bottom: Console(),
+          ratio: 0.85,
+          top: Results(),
         ),
       ),
     );
